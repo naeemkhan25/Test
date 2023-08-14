14bc7bb84f417f9f1077dfbe5dca79d4d8fdbb77
naeemkhan25 <naeemkhan.cse@gmail.com>
2023-08-14 10:39:09 +0600

    update

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
 echo "# Test" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/naeemkhan25/Test.git
-git push -u origin main
